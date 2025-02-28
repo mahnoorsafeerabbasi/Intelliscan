e83652dea5b28d35ab3e61a402451fe0a34cf8e9
mahnoorsafeerabbasi <129737455+mahnoorsafeerabbasi@users.noreply.github.com>
2025-02-28 09:56:52 +0500

    Update config.py

--- Intelliscan ServerSide/intellibot-fastapi/app/core/config.py
+++ Intelliscan ServerSide/intellibot-fastapi/app/core/config.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configuration settings
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "c6c34888-213a-4c44-9445-659d84763224")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "YOUR-API-KEY")
 INDEX_NAME = os.getenv("INDEX_NAME", "intelliscan-chatbot")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyC7aHCqal7gJEDItUaQi-WMv-J4Z2uUA7Q")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "YOUR-API-KEY")
