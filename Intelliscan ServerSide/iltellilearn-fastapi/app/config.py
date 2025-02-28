f37664049da920baf79dea875d7074a34da60629
mahnoorsafeerabbasi <129737455+mahnoorsafeerabbasi@users.noreply.github.com>
2025-02-28 09:48:39 +0500

    Update config.py

--- Intelliscan ServerSide/iltellilearn-fastapi/app/config.py
+++ Intelliscan ServerSide/iltellilearn-fastapi/app/config.py
@@ -1,14 +1,14 @@
 # Configuration for API keys and endpoints
 
 # Google Custom Search API
-GOOGLE_API_KEY = "AIzaSyD0NXOcSiBRLY1b5lIsEHFCp746DPYn2kE"
-GOOGLE_SEARCH_ENGINE_ID = "b32b71103c5394005"
+GOOGLE_API_KEY = "API_KEY"
+GOOGLE_SEARCH_ENGINE_ID = "ENGINE_ID"
 GOOGLE_SEARCH_URL = "https://www.googleapis.com/customsearch/v1"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 
 # GitHub API
-GITHUB_TOKEN = "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"
+GITHUB_TOKEN = "API_KEY"
 GITHUB_API_URL = "https://api.github.com/search/repositories"
 
 # Gemini API
-GEMINI_API_KEY = "AIzaSyCE64nokUEjV9TCkG6yC0HcH1FH7L3hpoI"
+GEMINI_API_KEY = "API_KEY"
